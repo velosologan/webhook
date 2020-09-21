ab05efe083cc38accc323dd2fc1d5d61f2905b72
Douglas Ramiro <douglas@cf.coop.br>
2020-09-21 08:47:45 -0300

    adicionado app

---
+++ src/test/java/br/com/tecnisys/webhook/WebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecnisys.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
